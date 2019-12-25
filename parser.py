13153c567207c549bdd3c46bb0a91d159de76d03
kasheen8 <abinaevravil@mail.ru>
2019-12-26 01:59:27 +0400

    Найден и спарсен нужный AJAX запрос Добавлен файл документации

--- parser.py
+++ parser.py
@@ -1,30 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import urllib.request #библиотека для работы с сетью
 import requests
 from bs4 import BeautifulSoup as bs #библиотека для парсинга html
 import lxml
+import json
 
 headers = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
            }
 
-base_url = 'https://betcity.ru/ru/line/culture/5325'
+base_url = 'https://ad.betcity.ru/d/off/events?rev=6&ver=48&csn=ooca9s'
 
-def get_html(url):
-    response = urllib.request.urlopen(url)
-    return response.read()
 
 def betcity_pars(base_url, headers):
     session = requests.Session()
-    request = session.get(base_url, headers=headers)
+    request = session.post(base_url, headers=headers, params = {'ids' : 5326})
     if request.status_code == 200:
-        print(request.content)
-        soup = bs(request.content,'lxml')
-        # div = soup.find_all('div',atrrs={'class':'container'})
-        print(soup)
-        # print('-' * 200)
-        # print(soup2)
+        print(json.loads(request.text))
     else:
         print('ERROR')
 
